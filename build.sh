a744e77b4ac73cde0ea8e032de7f5b3c0071df90
Jannis R <mail@jannisr.de>
2022-02-15 14:53:27 +0100

    use gtfs-hub DELFI.BB GTFS feed instead of VBB

--- build.sh
+++ build.sh
@@ -4,24 +4,23 @@ set -e
 set -o pipefail
 set -x
 
-wget --compression auto \
-	-r --no-parent --no-directories -R .csv.gz \
-	-P gtfs -N 'https://vbb-gtfs.jannisr.de/latest/'
+wget -c --compression auto -O gtfs.zip -N 'https://gtfs.mfdz.de/DELFI.BB.gtfs.zip'
+unzip -o -d gtfs -j gtfs.zip
 
 env | grep '^PG'
 
 NODE_ENV=production node_modules/.bin/gtfs-to-sql -d --trips-without-shape-id --routes-without-agency-id -- \
-	gtfs/agency.csv \
-	gtfs/calendar.csv \
-	gtfs/calendar_dates.csv \
-	gtfs/frequencies.csv \
-	gtfs/routes.csv \
-	gtfs/shapes.csv \
-	gtfs/stop_times.csv \
-	gtfs/stops.csv \
-	gtfs/transfers.csv \
-	gtfs/trips.csv \
+	gtfs/agency.txt \
+	gtfs/calendar.txt \
+	gtfs/calendar_dates.txt \
+	gtfs/routes.txt \
+	gtfs/shapes.txt \
+	gtfs/stop_times.txt \
+	gtfs/stops.txt \
+	gtfs/trips.txt \
 	| psql -b
+	# gtfs/frequencies.txt \
+	# gtfs/transfers.txt \
 
 lib="$(dirname $(realpath $0))/lib"
 NODE_ENV=production node_modules/.bin/build-gtfs-match-index \
