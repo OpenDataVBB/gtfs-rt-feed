b46edf687e988ad25fef5c852abd702c418a99df
Jannis R <mail@jannisr.de>
2022-02-17 20:04:20 +0100

    start.sh: don't run with NODE_ENV=production

--- start.sh
+++ start.sh
@@ -9,17 +9,17 @@ lib="$(dirname $(realpath $0))/lib"
 # https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/2173421#2173421
 trap 'exit_code=$?; kill -- $(jobs -p); exit $exit_code' SIGINT SIGTERM EXIT
 
-NODE_ENV=production HAFAS_MAX_RADAR_RESULTS=1000 node_modules/.bin/monitor-hafas \
+HAFAS_MAX_RADAR_RESULTS=1000 node_modules/.bin/monitor-hafas \
 	--movements-fetch-interval-fn $lib/movements-fetch-interval.js \
 	--trips-fetch-mode on-demand \
 	$lib/hafas.js \
 	&
 
-NODE_ENV=production node_modules/.bin/match-with-gtfs \
+node_modules/.bin/match-with-gtfs \
 	$lib/hafas-info.js $lib/gtfs-info.js \
 	&
 
-NODE_ENV=production node_modules/.bin/serve-as-gtfs-rt \
+node_modules/.bin/serve-as-gtfs-rt \
 	--signal-demand \
 	--static-feed-url 'https://gtfs.mfdz.de/DELFI.BB.gtfs.zip' \
 	&
