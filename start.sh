cc87966b7dc26e53c93a5e615ea3b8fd64ec58cc
Jannis R <mail@jannisr.de>
2022-02-22 20:44:50 +0100

    fetch movements less often during the night

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ lib="$(dirname $(realpath $0))/lib"
 trap 'exit_code=$?; kill -- $(jobs -p); exit $exit_code' SIGINT SIGTERM EXIT
 
 NODE_ENV=production node_modules/.bin/monitor-hafas \
+	--movements-fetch-interval-fn $lib/movements-fetch-interval.js \
 	--trips-fetch-mode on-demand \
 	$lib/hafas.js \
 	&
