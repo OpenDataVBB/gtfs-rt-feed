98d5490aa91484a4d8ad9c1962e845f709777d22
Jannis R <mail@jannisr.de>
2024-06-27 16:05:43 +0200

    match VDV AUS IstFahrts as GTFS trips

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import {createLogger} from './lib/logger.js'
 import {connectToNats} from './lib/nats.js'
+import {runGtfsMatching} from './lib/match.js'
 import {withSoftExit} from './lib/soft-exit.js'
+
+const logger = createLogger('service')
 
 const natsLogger = createLogger('nats')
 const {natsClient} = await connectToNats({
 	logger: natsLogger,
 })
 
-// todo: match with gtfs
+runGtfsMatching({
+	natsClient,
+})
 
 withSoftExit(() => {
 	natsClient.drain()
