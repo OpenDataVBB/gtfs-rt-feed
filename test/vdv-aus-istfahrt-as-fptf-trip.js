daa804b616d794a08e56b7227fd7ed715a77479e
Jannis R <mail@jannisr.de>
2024-08-24 19:36:20 +0200

    AUS IstFahrt -> FPTF trip: refactor ✅

--- test/vdv-aus-istfahrt-as-fptf-trip.js
+++ test/vdv-aus-istfahrt-as-fptf-trip.js
@@ -5,19 +5,28 @@ const require = createRequire(import.meta.url)
 
 import test from 'node:test'
 import {deepStrictEqual} from 'node:assert'
-import {formatVdvAusIstfahrtAsFptfTrip} from '../lib/vdv-aus-istfahrt-as-fptf-trip.js'
+import {createLogger} from '../lib/logger.js'
+import {createFormatVdvAusIstFahrtAsFptfTrip} from '../lib/vdv-aus-istfahrt-as-fptf-trip.js'
 
 const ausIstFahrt687 = require('./fixtures/aus-istfahrt-13865-00024-1#HVG.json')
 const fptfTrip687 = require('./fixtures/fptf-trip-13865-00024-1#HVG.json')
 const ausIstFahrt981 = require('./fixtures/aus-istfahrt-17638-00054-1#SVF.json')
 const fptfTrip981 = require('./fixtures/fptf-trip-17638-00054-1#SVF.json')
 
+const {
+	formatVdvAusIstFahrtAsFptfTrip,
+} = createFormatVdvAusIstFahrtAsFptfTrip({
+	logger: createLogger('vdv-aus-istfahrt-as-fptf-trip-test', {
+		level: 'fatal',
+	})
+})
+
 test('correctly formats AUS IstFahrt 13865-00024-1#HVG', (t) => {
-	const fptfTrip = formatVdvAusIstfahrtAsFptfTrip(ausIstFahrt687)
+	const fptfTrip = formatVdvAusIstFahrtAsFptfTrip(ausIstFahrt687)
 	deepStrictEqual(fptfTrip, fptfTrip687)
 })
 
 test('correctly formats AUS IstFahrt 17638-00054-1#SVF', (t) => {
-	const fptfTrip = formatVdvAusIstfahrtAsFptfTrip(ausIstFahrt981)
+	const fptfTrip = formatVdvAusIstFahrtAsFptfTrip(ausIstFahrt981)
 	deepStrictEqual(fptfTrip, fptfTrip981)
 })
