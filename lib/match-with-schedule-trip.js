OpenDataVBB/gtfs-rt-feed

commit 3595a1607c0acfd892dee7928daf782fa77ed3fb

matching: properly filter using trip_id/date pairs 🐛
fixes #5

lib/match-with-schedule-trip.js
@@ -195,8 +195,11 @@ SELECT
 	t_departure
 FROM arrivals_departures ad
 WHERE True
-AND trip_id = (SELECT trip_id FROM matches)
-AND "date" = (SELECT "date" FROM matches)
+-- Note: Using two separate \`= ANY()\` filters *is not* equivalent to an \`IN\` filter on the (trip_id, date) pairs!
+AND (ad.trip_id, ad.date) IN (
+	SELECT trip_id, "date"
+	FROM matches
+)
 ORDER BY trip_id, "date", stop_sequence_consec
 `
 
@@ -333,8 +336,11 @@ SELECT
 	t_departure
 FROM arrivals_departures ad
 WHERE True
-AND trip_id = (SELECT trip_id FROM matches)
-AND "date" = (SELECT "date" FROM matches)
+-- Note: Using two separate \`= ANY()\` filters *is not* equivalent to an \`IN\` filter on the (trip_id, date) pairs!
+AND (ad.trip_id, ad.date) IN (
+	SELECT trip_id, "date"
+	FROM matches
+)
 ORDER BY trip_id, "date", stop_sequence_consec
 `,
 		params: [

commit 9bcc8e46945107e1a96d65f612df72c1404d2818

matching: speed up filter using trip_id/date pairs ⚡️
This fixes the performance regression introduced by 3595a16 by circumventing the performance pitfalls.

related: #5

Thanks for the help @stultiloquence!

lib/match-with-schedule-trip.js
@@ -195,10 +195,19 @@ SELECT
 	t_departure
 FROM arrivals_departures ad
 WHERE True
--- Note: Using two separate \`= ANY()\` filters *is not* equivalent to an \`IN\` filter on the (trip_id, date) pairs!
+-- We can't use a mere \`trip_id = ANY(SELECT trip_id FROM matches)\` because, as of v14, PostgreSQL fails to "push down" the join/filtering [2] into \`arrivals_departures\` even though \`matches\` is materialized and known to be small. By forcing PostgreSQL to "collect" the values from \`matches\` using \`array()\` [0][1], we guide it to todo. (The same applies to \`date\`.)
+-- This technique *does not* work (i.e. is slow) when using \`IN\` to filter with (trip_id, date) pairs – using two separate \`= ANY()\` filters is not equivalent, after all! –, so we first filter using \`= ANY()\` on trip_id & date for speed, and then filter on the pairs for correctness.
+-- [0] https://stackoverflow.com/a/15007154/1072129
+-- [1] https://dba.stackexchange.com/a/189255/289704
+-- [2] https://stackoverflow.com/a/66626205/1072129
+AND ad.trip_id = ANY(array(SELECT trip_id FROM matches))
+AND ad.date = ANY(array(SELECT "date" FROM matches))
 AND (ad.trip_id, ad.date) IN (
-	SELECT trip_id, "date"
-	FROM matches
+	SELECT *
+	FROM unnest(
+		array(SELECT trip_id FROM matches),
+		array(SELECT "date" FROM matches)
+	) AS t(trip_id, "date")
 )
 ORDER BY trip_id, "date", stop_sequence_consec
 `
@@ -336,10 +345,19 @@ SELECT
 	t_departure
 FROM arrivals_departures ad
 WHERE True
--- Note: Using two separate \`= ANY()\` filters *is not* equivalent to an \`IN\` filter on the (trip_id, date) pairs!
+-- We can't use a mere \`trip_id = ANY(SELECT trip_id FROM matches)\` because, as of v14, PostgreSQL fails to "push down" the join/filtering [2] into \`arrivals_departures\` even though \`matches\` is materialized and known to be small. By forcing PostgreSQL to "collect" the values from \`matches\` using \`array()\` [0][1], we guide it to todo. (The same applies to \`date\`.)
+-- This technique *does not* work (i.e. is slow) when using \`IN\` to filter with (trip_id, date) pairs – using two separate \`= ANY()\` filters is not equivalent, after all! –, so we first filter using \`= ANY()\` on trip_id & date for speed, and then filter on the pairs for correctness.
+-- [0] https://stackoverflow.com/a/15007154/1072129
+-- [1] https://dba.stackexchange.com/a/189255/289704
+-- [2] https://stackoverflow.com/a/66626205/1072129
+AND ad.trip_id = ANY(array(SELECT trip_id FROM matches))
+AND ad.date = ANY(array(SELECT "date" FROM matches))
 AND (ad.trip_id, ad.date) IN (
-	SELECT trip_id, "date"
-	FROM matches
+	SELECT *
+	FROM unnest(
+		array(SELECT trip_id FROM matches),
+		array(SELECT "date" FROM matches)
+	) AS t(trip_id, "date")
 )
 ORDER BY trip_id, "date", stop_sequence_consec
 `,