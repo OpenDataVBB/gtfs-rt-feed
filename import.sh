cedacf28ad99a0523fd208fa832c84b00d8466f1
Jannis R <mail@jannisr.de>
2024-10-03 22:17:06 +0200

    GTFS import: let Docker-based import use postgis-gtfs-importer:v4
    see also https://github.com/mobidata-bw/postgis-gtfs-importer/commits/e93860f5d17069175904b05c98e44869856d3d8a/
    
    follow-up of 715a2b6

--- import.sh
+++ import.sh
@@ -7,7 +7,6 @@ export GTFS_DOWNLOAD_USER_AGENT="${GTFS_DOWNLOAD_USER_AGENT:-OpenDataVBB/gtfs-rt
 export GTFS_DOWNLOAD_URL="${GTFS_DOWNLOAD_URL:-https://www.vbb.de/vbbgtfs}"
 export GTFS_IMPORTER_DB_PREFIX="${GTFS_IMPORTER_DB_PREFIX:-gtfs}"
 export GTFS_TMP_DIR="${GTFS_TMP_DIR:-"$PWD/gtfs"}"
-export GTFS_IMPORTER_SCHEMA=public
 export GTFS_POSTPROCESSING_D_PATH="${GTFS_POSTPROCESSING_D_PATH:-"$PWD/gtfs-postprocessing.d"}"
 
 set -x
@@ -24,10 +23,9 @@ if [ "${1:-}" = '--docker' ]; then
 		-e GTFS_DOWNLOAD_URL \
 		-e GTFS_IMPORTER_DB_PREFIX \
 		-e GTFS_TMP_DIR \
-		-e GTFS_IMPORTER_SCHEMA \
-		-e GTFS_SQL_D_PATH=/etc/gtfs-postprocessing.d \
+		-e GTFS_POSTPROCESSING_D_PATH=/etc/gtfs-postprocessing.d \
 		-e PGHOST=localhost -e PGUSER -e PGPASSWORD -e PGDATABASE \
-		ghcr.io/mobidata-bw/postgis-gtfs-importer:v3
+		ghcr.io/mobidata-bw/postgis-gtfs-importer:v4
 else
 	# run PostGIS GTFS importer locally
 	./postgis-gtfs-importer/importer.js
