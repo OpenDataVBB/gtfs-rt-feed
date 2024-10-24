06a23a275c0634beecb9d5d57db509bf17108e0d
Jannis R <mail@jannisr.de>
2024-10-24 18:22:41 +0200

    GTFS import: disable gtfstidy-ing by default

--- import.sh
+++ import.sh
@@ -8,6 +8,8 @@ export GTFS_DOWNLOAD_URL="${GTFS_DOWNLOAD_URL:-https://www.vbb.de/vbbgtfs}"
 export GTFS_IMPORTER_DB_PREFIX="${GTFS_IMPORTER_DB_PREFIX:-gtfs}"
 export GTFS_TMP_DIR="${GTFS_TMP_DIR:-"$PWD/gtfs"}"
 export GTFS_POSTPROCESSING_D_PATH="${GTFS_POSTPROCESSING_D_PATH:-"$PWD/gtfs-postprocessing.d"}"
+# The VBB GTFS feed usually doesn't need gtfstidy-ing.
+export GTFSTIDY_BEFORE_IMPORT="${GTFSTIDY_BEFORE_IMPORT:-false}"
 
 # if stdin is not a TTY, don't pass `-it`
 docker_run_args=()
@@ -29,6 +31,7 @@ if [ "${1:-}" = '--docker' ]; then
 		-e GTFS_DOWNLOAD_URL \
 		-e GTFS_IMPORTER_DB_PREFIX \
 		-e GTFS_TMP_DIR \
+		-e GTFSTIDY_BEFORE_IMPORT  \
 		-e GTFS_POSTPROCESSING_D_PATH=/etc/gtfs-postprocessing.d \
 		-e PGHOST=localhost -e PGUSER -e PGPASSWORD -e PGDATABASE \
 		ghcr.io/mobidata-bw/postgis-gtfs-importer:v4
