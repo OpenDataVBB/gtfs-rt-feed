26edf32af09044b2c9439b117c0f9a6c4a613caa
Jannis R <mail@jannisr.de>
2026-06-04 23:14:47 +0200

    chore: prepare for >1 test datasets ✅

--- test/index.sh
+++ test/index.sh
@@ -4,15 +4,22 @@ set -eu -o pipefail
 cd "$(dirname $0)"
 set -x
 
+node vdv-merging.js
+
+export MATCH_GTFS_RT_TO_GTFS_CACHING=false
+
+# ---
+
+# VBB 2024-06-21 test
+
+psql -c 'CREATE DATABASE test_vbb_2024'
+export PGDATABASE=test_vbb_2024
 env \
 	GTFS_TMP_DIR="$PWD/gtfs" \
 	GTFS_DOWNLOAD_URL='https://vbb-gtfs.jannisr.de/2024-06-21.gtfs.zip' \
 	GTFS_IMPORTER_SCHEMA=public \
+	GTFS_IMPORTER_DB_PREFIX=vbb_2024 \
 	../import.sh
 
-# ---
-
-export PGDATABASE="$(psql -q --csv -t -c 'SELECT db_name FROM latest_successful_imports')"
-
-node vdv-merging.js
-env MATCH_GTFS_RT_TO_GTFS_CACHING=false node matching.js
+env PGDATABASE="$(psql -q --csv -t -c 'SELECT db_name FROM latest_successful_imports')" \
+	node matching-vbb-2024.js
