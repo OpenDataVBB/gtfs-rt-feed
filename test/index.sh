f6edb43bfc808a9365e1aed87fa2555a823f0e31
Jannis R <mail@jannisr.de>
2026-09-01 22:22:12 +0200

    run quick tests first & slow tests last ✅

--- test/index.sh
+++ test/index.sh
@@ -10,22 +10,6 @@ export MATCH_GTFS_RT_TO_GTFS_CACHING=false
 
 # ---
 
-# VBB 2024-06-21 test
-
-psql -c 'CREATE DATABASE test_vbb_2024'
-export PGDATABASE=test_vbb_2024
-env \
-	GTFS_TMP_DIR="$PWD/gtfs" \
-	GTFS_DOWNLOAD_URL='https://vbb-gtfs.jannisr.de/2024-06-21.gtfs.zip' \
-	GTFS_IMPORTER_SCHEMA=public \
-	GTFS_IMPORTER_DB_PREFIX=vbb_2024 \
-	../import.sh
-
-env PGDATABASE="$(psql -q --csv -t -c 'SELECT db_name FROM latest_successful_imports')" \
-	node matching-vbb-2024.js
-
-# ---
-
 # VBB 2026-06-03 (filtered to only one M5 trip) test
 
 psql -c 'CREATE DATABASE test_vbb_m5'
@@ -89,3 +73,19 @@ env \
 env PGDATABASE="$(psql -q --csv -t -c 'SELECT db_name FROM latest_successful_imports')" \
 	VDV_FAHRTS_INCLUDE_SERVICE_STOPS=false \
 	node matching-vbb-re3.js
+
+# ---
+
+# VBB 2024-06-21 test
+
+psql -c 'CREATE DATABASE test_vbb_2024'
+export PGDATABASE=test_vbb_2024
+env \
+	GTFS_TMP_DIR="$PWD/gtfs" \
+	GTFS_DOWNLOAD_URL='https://vbb-gtfs.jannisr.de/2024-06-21.gtfs.zip' \
+	GTFS_IMPORTER_SCHEMA=public \
+	GTFS_IMPORTER_DB_PREFIX=vbb_2024 \
+	../import.sh
+
+env PGDATABASE="$(psql -q --csv -t -c 'SELECT db_name FROM latest_successful_imports')" \
+	node matching-vbb-2024.js
